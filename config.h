11ec76bbf51e9cdfbc3ad7cee5539c806b9726a4
sweathrt <sweathrt.d@gmail.com>
2022-07-21 21:29:30 +0300

    changed configuration slightly

--- config.h
+++ config.h
@@ -6,8 +6,18 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "JetBrains Mono:size=12" };
-static const char dmenufont[]       = "JetBrains Mono:size=12";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+// static const char col_gray1[]       = "#222222";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
+// static const char col_cyan[]        = "#005577";
+// static const char *colors[][3]      = {
+//         /*               fg         bg         border   */
+//         [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//         [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+// };
 
 #include "/home/sweathrt/.cache/wal/colors-wal-dwm.h"
 
@@ -21,7 +31,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
